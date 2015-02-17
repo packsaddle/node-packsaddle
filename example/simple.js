e566461a8c957a39de65668410ddcc4658e09ca1
sanemat <o.gata.ken@gmail.com>
2015-02-18 01:18:22 +0900

    Initial commit

---
+++ example/simple.js
@@ -0,0 +1,13 @@
+/*
+ * saddler
+ * https://github.com/sanemat/saddler
+ *
+ * Copyright (c) 2015 sanemat
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+var saddler = require('../');
+
+console.log(saddler()); // "awesome"
---
+++ test/saddler_test.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var saddler = require('../');
+var assert = require('should');
+
+describe('saddler', function () {
+
+  it('should be awesome', function () {
+    saddler().should.equal('awesome');
+  });
+
+});
